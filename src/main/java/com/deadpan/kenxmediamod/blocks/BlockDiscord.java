40ac34a2e291ea02a9b68b90bb599a0dd15a52b7
Deadpan <benjamin.t.j.beacham@gmail.com>
2022-07-15 21:14:08 +0100

    Create BlockDiscord.java

---
+++ src/main/java/com/deadpan/kenxmediamod/blocks/BlockDiscord.java
@@ -0,0 +1,10 @@
+package com.deadpan.kenxmediamod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockDiscord extends Block {
+    public BlockDiscord(Material material) {
+        super(material);
+    }
+}
